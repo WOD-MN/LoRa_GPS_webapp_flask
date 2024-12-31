f0e320483255327bf3dd2d396ee6349a9c62a9cd
saiman pokhrel <70017194+WOD-MN@users.noreply.github.com>
2024-12-31 17:48:02 +0545

    Update LoRaReceiver.ino

--- LoRaReceiver/LoRaReceiver.ino
+++ LoRaReceiver/LoRaReceiver.ino
@@ -13,7 +13,7 @@
 #define SS      18
 #define RST     14
 #define DI0     26
-#define BAND    915E6  // Set your frequency
+#define BAND    868E6  // Set your frequency
 
 // OLED display width and height
 #define SCREEN_WIDTH 128
